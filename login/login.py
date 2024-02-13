dd83ad6fb0786d1f0612aedae6c14f54bcf2ff5c
ericmux <ericgmconrado@gmail.com>
2024-02-13 12:12:33 +0100

    Remove login debug.

--- login/login.py
+++ login/login.py
@@ -5,11 +5,9 @@ import os
 def create_tg_client(config_path :str):
     path = os.path.abspath(config_path)
     try:
-        with open(os.path.abspath(config_path)) as file:
+        with open(path) as file:
             kw_dict = json.loads(file.read().rstrip())
-            print(kw_dict)
             tg = Telegram(**kw_dict)
-            print(dir(tg))
             return tg
     except:
         print("Failure while trying to read file.")
